640b36919957c262f6cd55dc8e3d8feedf3e333b
keer4n <keer4n@protonmail.com>
2020-09-18 18:58:09 -0600

    Updates

---
+++ precache-manifest.133d7f54ace950ccae19d1547404f6cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "823a94a342e609dcae204cc06884cc64",
+    "url": "/ref-vision/index.html"
+  },
+  {
+    "revision": "c99a1dd56daf8f70df7a",
+    "url": "/ref-vision/static/css/main.d4f9f685.chunk.css"
+  },
+  {
+    "revision": "c60eb2ff8bc5f2500230",
+    "url": "/ref-vision/static/js/2.9f0a6aeb.chunk.js"
+  },
+  {
+    "revision": "ee8909476e1daa379d46b6d7286404f2",
+    "url": "/ref-vision/static/js/2.9f0a6aeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c99a1dd56daf8f70df7a",
+    "url": "/ref-vision/static/js/main.18900bad.chunk.js"
+  },
+  {
+    "revision": "5ca1c79d563ca38d3a27",
+    "url": "/ref-vision/static/js/runtime-main.a9eda20d.js"
+  }
+]);
